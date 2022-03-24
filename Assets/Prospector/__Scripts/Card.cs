5db2df44b7ffd7323f417f24bd692e32a4b058fd
Chuyuan <cc1705@scarletmail.rutgers.edu>
2022-03-23 20:51:53 -0400

    Card script update

--- Assets/Prospector/__Scripts/Card.cs
+++ Assets/Prospector/__Scripts/Card.cs
@@ -13,8 +13,35 @@ public class Card : MonoBehaviour {
 	public List<GameObject> pipGOs = new List<GameObject>();
 	
 	public GameObject back;  // back of card;
-	public CardDefinition def;  // from DeckXML.xml		
+	public CardDefinition def;  // from DeckXML.xml	
+	public SpriteRenderer[]	spriteRenderers;
 
+	void Start() {
+		SetSortOrder(0);
+	}
+	public void PopulatedSpriteRenderers(){
+		if(spriteRenderers == null || spriteRenderers.Length == 0) {
+			spriteRenderers = GetComponentsInChildren<spriteRenderers>();
+		}
+	}
+	public void SetSortingLayerName(string tSLN) {
+		PopulatedSpriteRenderers();
+		foreach (SpriteRenderer tSR in spriteRenderers) {
+			if (tSR.gameObject == this.gameObject) {
+				tSR.sortingOrder = s0rd;
+				continue;
+			}
+		switch (tSR.gameObject.name) {
+			case "back": // if the name is "back"
+			tSR.sortingOrder = SortedList+2;
+			break;
+			case "face":
+			default: //or if it's anything else
+			tSR.sortingOrder = sOrd+1;
+			break;
+		}
+	  }
+	}
 
 	public bool faceUp {
 		get {
@@ -25,19 +52,6 @@ public class Card : MonoBehaviour {
 			back.SetActive(!value);
 		}
 	}
-
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-} // class Card
-
 [System.Serializable]
 public class Decorator{
 	public string	type;			// For card pips, tyhpe = "pip"
@@ -52,4 +66,6 @@ public class CardDefinition{
 	public int		rank;	// value from 1-13 (Ace-King)
 	public List<Decorator>	
 					pips = new List<Decorator>();  // Pips Used
+
+}
 }
